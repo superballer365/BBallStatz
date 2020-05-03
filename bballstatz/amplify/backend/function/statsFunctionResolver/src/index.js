2b79ee1515585655fac991dcb46c5ad080b790c1
Matthew Balnis <matthew.balnis@uconn.edu>
2020-05-02 21:35:12 -0400

    Now computing actual stats for the specified player

--- bballstatz/amplify/backend/function/statsFunctionResolver/src/index.js
+++ bballstatz/amplify/backend/function/statsFunctionResolver/src/index.js
@@ -1,15 +1,61 @@
-function getPlayerStats() {
+const http = require("http");
+
+function calculatePerGameStats(playerData) {
+  let totalGames = 0;
+  let totalPoints = 0;
+  let totalAssists = 0;
+  let totalRebounds = 0;
+  // getting stats from most recent season by default
+  playerData.seasons[0].teams.forEach(team => {
+    totalGames += team.total.games_played;
+    totalPoints += team.total.points;
+    totalAssists += team.total.assists;
+    totalRebounds += team.total.rebounds;
+  });
+
   return {
-    points: 30.1,
-    rebounds: 6.3,
-    assists: 5.8
+    points: totalPoints / totalGames,
+    rebounds: totalRebounds / totalGames,
+    assists: totalAssists / totalGames
   };
+}
+
+function getPlayerStats(playerId) {
+  return new Promise((resolve, reject) => {
+    const req = http.request(
+      `http://api.sportradar.us/nba/trial/v5/en/players/${playerId}/profile.json?api_key=4kfpcdnf83bx43bj9gjucfym`,
+      res => {
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+          return reject(new Error("statusCode=" + res.statusCode));
+        }
+
+        let perGameStats = undefined;
+        let body = [];
+        res.on("data", function(chunk) {
+          body.push(Buffer.from(chunk));
+        });
+        res.on("end", function() {
+          try {
+            const statsData = JSON.parse(Buffer.concat(body).toString());
+            perGameStats = calculatePerGameStats(statsData);
+          } catch (e) {
+            reject(e);
+          }
+          resolve(perGameStats);
+        });
+      }
+    );
+    req.on("error", e => {
+      reject(e.message);
+    });
+    req.end();
+  });
 }
 
 const resolvers = {
   Player: {
     perGameStats: ctx => {
-      return getPlayerStats();
+      return getPlayerStats(ctx.source.id);
     }
   }
 };
