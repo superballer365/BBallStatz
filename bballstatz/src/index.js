91f2ee3bca138a4799aceb1ca2eadf1a1af71b83
Matthew Balnis <matthew.balnis@uconn.edu>
2020-05-31 10:53:34 -0400

    Added some logging

--- bballstatz/src/index.js
+++ bballstatz/src/index.js
@@ -8,6 +8,8 @@ import Amplify from "aws-amplify";
 
 // set auth type for AppSync API to be API Key
 awsmobile.aws_appsync_authenticationType = "API_KEY";
+console.log("Amplify config:");
+console.log(awsmobile);
 Amplify.configure(awsmobile);
 
 ReactDOM.render(
