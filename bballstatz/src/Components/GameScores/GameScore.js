60dda6d8b3f0d25df135b0aed7e8fbfdae80da5f
Matthew Balnis <matthew.balnis@uconn.edu>
2020-06-28 20:33:12 -0400

    Starting to render a very basic game score list

---
+++ bballstatz/src/Components/GameScores/GameScore.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import * as queries from "../../graphql/queries";
+import * as customQueries from "../../graphql/customQueries";
+import Button from "@material-ui/core/Button";
+import Loader from "react-loader-spinner";
+import "./GameScore.css";
+
+function GameScore(props) {
+  const {
+    id,
+    date,
+    homeTeam,
+    awayTeam,
+    homeScore,
+    awayScore,
+    period,
+    isOver,
+    postSeason
+  } = props;
+
+  return (
+    <div className="container">
+      {`${homeTeam.abbreviation}: ${homeScore}    ${awayTeam.abbreviation}: ${awayScore}`}
+    </div>
+  );
+}
+
+export default GameScore;
--- bballstatz/src/Components/GameScores/GameScores.js
+++ bballstatz/src/Components/GameScores/GameScores.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import * as queries from "../../graphql/queries";
+import GameScore from "./GameScore";
+import * as customQueries from "../../graphql/customQueries";
+import Button from "@material-ui/core/Button";
+import Loader from "react-loader-spinner";
 import "./GameScores.css";
 
 function GameScores() {
+  const [gameScores, setGameScores] = useState([]);
+  const [isLoadingGameScores, setIsLoadingGameScores] = useState(false);
+
+  useEffect(() => {
+    const fetchGameScores = async () => {
+      setIsLoadingGameScores(true);
+      const gameScores = await API.graphql(
+        graphqlOperation(queries.getGameScores, {
+          date: new Date().toISOString()
+        })
+      );
+
+      setGameScores(gameScores.data.getGameScores);
+      setIsLoadingGameScores(false);
+    };
+
+    fetchGameScores();
+  }, []);
+
+  console.log(gameScores);
   return (
     <div className="container">
       <h1>Game Scores</h1>
-      <p>Coming soon...</p>
+      {isLoadingGameScores ? (
+        <Loader type="TailSpin" color="#00BFFF" height={100} width={100} />
+      ) : (
+        <div className="gameScoresContainer">
+          {gameScores.map(gameScore => (
+            <GameScore
+              key={gameScore.id}
+              date={gameScore.date}
+              homeTeam={gameScore.homeTeam}
+              awayTeam={gameScore.awayTeam}
+              homeScore={gameScore.homeScore}
+              awayScore={gameScore.awayScore}
+              period={gameScore.period}
+              isOver={gameScore.isOver}
+              postSeason={gameScore.postSeason}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
