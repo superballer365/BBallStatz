bae2b6b9aed3c4dc3b54be09a10e92278ac8c401
Matthew Balnis <matthew.balnis@uconn.edu>
2020-12-25 20:42:09 -0500

    Added error handling and logic for case where box score data isn't available

--- bballstatz/src/Components/BoxScore/BoxScore.js
+++ bballstatz/src/Components/BoxScore/BoxScore.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback, useMemo } from "react";
+import React, { useEffect, useCallback, useReducer } from "react";
 import styles from "./BoxScore.module.css";
 import { API, graphqlOperation } from "aws-amplify";
 import * as queries from "../../graphql/queries";
@@ -10,9 +10,13 @@ import BoxScoreHeader from "./BoxScoreHeader";
 
 function BoxScore(props) {
   const gameId = props.match.params.gameId;
-  const [activeTeam, setActiveTeam] = useState();
-  const [boxScoreData, setBoxScoreData] = useState();
-  const [isLoadingBoxScore, setIsLoadingBoxScore] = useState(false);
+  const [state, dispatch] = useReducer(boxScoreReducer, {
+    boxScoreData: undefined,
+    activeTeam: undefined,
+    isLoadingBoxScore: true,
+    error: false
+  });
+  const { boxScoreData, activeTeam, isLoadingBoxScore, error } = state;
 
   useEffect(() => {
     const fetchBoxScore = async () => {
@@ -25,17 +29,15 @@ function BoxScore(props) {
       return boxScore.data.getBoxScore;
     };
 
-    setIsLoadingBoxScore(true);
+    dispatch({ type: "setLoading" });
     fetchBoxScore()
       .then(result => {
-        setBoxScoreData(result);
-        setActiveTeam(result.gameScore.homeTeam.abbreviation);
+        dispatch({ type: "setBoxScoreData", payload: result });
       })
       .catch(error => {
-        setBoxScoreData([]);
+        dispatch({ type: "setError" });
         console.error(error);
-      })
-      .finally(() => setIsLoadingBoxScore(false));
+      });
   }, [gameId]);
 
   const getTableData = useCallback(() => {
@@ -56,40 +58,74 @@ function BoxScore(props) {
     });
   }, [boxScoreData, activeTeam]);
 
-  return (
-    <div className={styles.container}>
-      {isLoadingBoxScore || !boxScoreData ? (
+  const getContent = () => {
+    if (isLoadingBoxScore) {
+      return (
         <Loading marginTop={100} size="medium" message="Loading box score..." />
-      ) : (
-        <>
-          <BoxScoreHeader data={boxScoreData.gameScore} />
-          <div className={styles.teamSelector}>
-            <ToggleButtonGroup
-              value={activeTeam}
-              exclusive
-              onChange={(event, newActiveTeam) => {
-                setActiveTeam(newActiveTeam);
-              }}
-            >
-              <ToggleButton
-                value={boxScoreData.gameScore.homeTeam.abbreviation}
-              >
-                {boxScoreData.gameScore.homeTeam.abbreviation}
-              </ToggleButton>
-              <ToggleButton
-                value={boxScoreData.gameScore.awayTeam.abbreviation}
-              >
-                {boxScoreData.gameScore.awayTeam.abbreviation}
-              </ToggleButton>
-            </ToggleButtonGroup>
-          </div>
-          <div className={styles.tableContainer}>
-            <BoxScoreTable data={getTableData()} />
-          </div>
-        </>
-      )}
-    </div>
-  );
+      );
+    }
+
+    if (!boxScoreData) {
+      return <div>No box score data available for this game</div>;
+    }
+
+    if (error) {
+      return <div>Failed to load box score data</div>;
+    }
+
+    return (
+      <>
+        <BoxScoreHeader data={boxScoreData.gameScore} />
+        <div className={styles.teamSelector}>
+          <ToggleButtonGroup
+            value={activeTeam}
+            exclusive
+            onChange={(event, newActiveTeam) => {
+              dispatch({ type: "setActiveTeam", payload: newActiveTeam });
+            }}
+          >
+            <ToggleButton value={boxScoreData.gameScore.homeTeam.abbreviation}>
+              {boxScoreData.gameScore.homeTeam.abbreviation}
+            </ToggleButton>
+            <ToggleButton value={boxScoreData.gameScore.awayTeam.abbreviation}>
+              {boxScoreData.gameScore.awayTeam.abbreviation}
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </div>
+        <div className={styles.tableContainer}>
+          <BoxScoreTable data={getTableData()} />
+        </div>
+      </>
+    );
+  };
+
+  return <div className={styles.container}>{getContent()}</div>;
 }
+
+const boxScoreReducer = (state, action) => {
+  switch (action.type) {
+    case "setLoading":
+      return { ...state, isLoadingBoxScore: true };
+    case "setBoxScoreData":
+      return {
+        ...state,
+        isLoadingBoxScore: false,
+        error: false,
+        boxScoreData: action.payload,
+        activeTeam: action.payload.gameScore.homeTeam.abbreviation
+      };
+    case "setActiveTeam":
+      return { ...state, activeTeam: action.payload };
+    case "setError":
+      return {
+        ...state,
+        error: true,
+        data: undefined,
+        isLoadingBoxScore: false
+      };
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+};
 
 export default BoxScore;
